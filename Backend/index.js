c3c3f3a4d7e7c308032ce8c32439b9ee0fc814b8
abp197 <150072353+abp197@users.noreply.github.com>
2025-01-01 23:38:46 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -40,7 +40,7 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-app.use(express.static(path.join(_dirname,"/Frontend/disto")))
+app.use(express.static(path.join(_dirname,"/Frontend/dist")))
 app.get('*',(req,res)=>{
     res.sendFile(path.resolve(_dirname , "Frontend" , "dist" , "index.html"))
 })
